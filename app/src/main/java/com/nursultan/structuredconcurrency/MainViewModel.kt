0d4e528ec4488b7b33f7ee9bd4186594c2d445a6
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-05-06 21:48:45 +0300

    imp coroutine exception handler)

--- app/src/main/java/com/nursultan/structuredconcurrency/MainViewModel.kt
+++ app/src/main/java/com/nursultan/structuredconcurrency/MainViewModel.kt
@@ -3,27 +3,47 @@ package com.nursultan.structuredconcurrency
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
+import java.lang.RuntimeException
 import kotlin.concurrent.thread
 
 class MainViewModel : ViewModel() {
     private val parentJob = Job()
+    private val coroutineEH = CoroutineExceptionHandler { _, throwable ->
+        Log.d(LOG_TAG, "Exception is: $throwable")
+    }
     private val coroutineScope = CoroutineScope(
-        Dispatchers.Main + CoroutineName("Main Coroutine") + parentJob
+        Dispatchers.Main
+                + CoroutineName("Main Coroutine")
+                + parentJob
+                + coroutineEH
     )
+    private val coroutineScopeWithException =
+        CoroutineScope(Dispatchers.Main + coroutineEH)
 
     fun start() {
         val childJob1 = coroutineScope.launch {
-            delay(1000)
+            coroutineScopeWithException.launch {
+                error()
+            }
+            delay(2000)
             Log.d(LOG_TAG, "childJob1 finished")
         }
         val childJob2 = coroutineScope.launch {
             delay(3000)
             Log.d(LOG_TAG, "childJob2 finished")
         }
+        val childJob3 = coroutineScope.launch {
+            delay(5000)
+            error()
+        }
         thread {
             Thread.sleep(4500)
             Log.d(LOG_TAG, parentJob.isActive.toString())
         }
+    }
+
+    private fun error() {
+        throw RuntimeException()
     }
 
     override fun onCleared() {
